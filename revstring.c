7a5421906e8454c00d2f9e964e09859aa13b58fe
neeraj-Tech-Wiz <upadhyeneeraj7777@gmail.com>
2024-02-17 19:01:18 +0530

    simple program to reverse a string without predefined functions

---
+++ revstring.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main(){
+    char str1[50],str2[50];
+    int count,i,length;
+    printf("Enter a string you want to get reverse\n");
+    gets(str1);
+    length=strlen(str1);
+    count=0;
+    for(i=length-1;i>=0;i--){
+        str2[count]=str1[i];
+        count++;
+    }
+    printf("Reverse string is\n");
+    puts(str2);
+}
